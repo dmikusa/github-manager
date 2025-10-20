681e47f85737626089ac487adf040c41d969a49a
Daniel Mikusa <dan@mikusa.com>
2025-10-20 12:54:51 -0400

    Allow location of config to be adjustable
    By specifying the `GHM_REPO_CONFIG`, you can change where it loads your repo config from. By default it's `~/.ghm/repos.json`. Set the value of the environment variable to the full path to the JSON file and it will use that one instead.
    
    Signed-off-by: Daniel Mikusa <dan@mikusa.com>

--- ghm/utils.py
+++ ghm/utils.py
@@ -2,7 +2,7 @@ import os
 import json
 from .runner import GhRunner
 
-REPO_CONFIG_LOCATION = os.path.expanduser('~/.ghm/repos.json')
+REPO_CONFIG_LOCATION = os.path.expanduser("~/.ghm/repos.json")
 
 
 def fetch_buildpack_toml(repo):
@@ -10,7 +10,8 @@ def fetch_buildpack_toml(repo):
     import subprocess
 
     fp = urllib.request.urlopen(
-        f"https://raw.githubusercontent.com/{repo}/main/buildpack.toml")
+        f"https://raw.githubusercontent.com/{repo}/main/buildpack.toml"
+    )
     cmd = ["yj", "-tj"]
     res = subprocess.run(cmd, capture_output=True, check=True, input=fp.read())
     return json.loads(res.stdout)
@@ -18,6 +19,7 @@ def fetch_buildpack_toml(repo):
 
 def check_requirements():
     from subprocess import CalledProcessError
+
     try:
         GhRunner().help()
         return True
@@ -29,10 +31,11 @@ def load_repos(remote_repos=False, org=None):
     """Loads a JSON formatted list of repositories to be used by the script"""
     if remote_repos:
         repos = GhRunner().list_repos(org=org)
-        return [repo['full_name'] for repo in repos
-                if 'full_name' in repo.keys()]
+        return [repo["full_name"] for repo in repos if "full_name" in repo.keys()]
     else:
-        repos = json.load(open(REPO_CONFIG_LOCATION))
+        repo_config_location = os.environ.get("GHM_REPO_CONFIG", REPO_CONFIG_LOCATION)
+
+        repos = json.load(open(repo_config_location))
         if not hasattr(repos, "append") or not hasattr(repos, "__len__"):
             raise TypeError("Invalid configuration file")
         return repos
